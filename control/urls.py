cfffb9b52abed247233ef4d817bde6262828f77d
reina91kr <jjangyunji@gmail.com>
2022-01-03 12:53:43 +0900

    'last

--- control/urls.py
+++ control/urls.py
@@ -5,5 +5,7 @@ app_name = 'control'
 
 urlpatterns = [
     path('', views.index, name='index'),       #127.0.0.1:8000/control
-    path('register/', views.register, name ="register")
+    path('register/', views.register, name ="register"),
+    path('counter/', views.counter, name='counter'),
+    path('calc_even_odd/', views.calc_even_odd, name='calc_even_odd'),
 ]
